7896861684c8efc5015dba16d3dc0c17e1a9f07f
Kishor Jadhao <kishorjadhao1990@gmail.com>
2021-07-22 16:08:44 +0530

    Eureka client config added

--- olx-master-data/src/main/java/com/zensar/OlxMesterdataApplication.java
+++ olx-master-data/src/main/java/com/zensar/OlxMesterdataApplication.java
@@ -2,12 +2,10 @@ package com.zensar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableMongoRepositories
 public class OlxMesterdataApplication {
 
     public static void main(String[] args) {
